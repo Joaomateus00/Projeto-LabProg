1341a1de1933d8945f0aefb3b2279843fb8efaa4
viccamposr <victoria.campos@ufv.br>
2026-03-25 19:09:07 -0300

    Finalizando back-end e banco de dados

--- backend/server.js
+++ backend/server.js
@@ -53,6 +53,39 @@ app.post('/register', async (req, res) => {
 });
 
 
+app.post('/login', async (req, res)=>{
+    const {email,senha} = req.body;
+
+    try{
+        const database = client.db('ClusterFacul');
+        const usuarios = database.collection('usuarios');
+
+        const userExists = await usuarios.findOne({ email });
+
+        if(userExists){
+            if(userExists.senha === senha){
+                return res.status(200).json({
+                    message: 'Login bem sucedido'
+                });
+            }else{
+                return res.status(400).json({
+                    message: 'senha incorreta'
+                });
+            }            
+        }else {
+                return res.status(400).json({
+                    message: 'usuario incorreto'
+                });
+            }
+
+    } catch (error) {
+        console.log("Erro no login", error);
+        res.status(500).json({
+            message: 'Erro interno no servidor'
+        });
+    }
+});
+
 
 const PORT = 3001;
 
